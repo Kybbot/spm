f89366a2d32a62542dbac580cbbf6af1c0ffbfea
Артём Одийко <odiykoartem@gmail.com>
2023-08-22 11:35:02 +0300

    Created Header component

---
+++ src/widgets/Header/config/navItems.ts
@@ -0,0 +1,15 @@
+export const navItems = {
+	client: [
+		{ link: "/", name: "Home" },
+		{ link: "/advice", name: "Advice" },
+		{ link: "/live-portfolio", name: "Live Portfolio" },
+		{ link: "/documents", name: "Documents" },
+	],
+	admin: [
+		{ link: "/advice", name: "Advice" },
+		{ link: "/actions", name: "Actions" },
+		{ link: "/clients", name: "Clients" },
+		{ link: "/admins", name: "Admins" },
+		{ link: "/documents", name: "Documents" },
+	],
+};
