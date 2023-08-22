3ed21c28cea5757d564f59c5165927196f76b513
Артём Одийко <odiykoartem@gmail.com>
2023-08-22 17:11:44 +0300

    Created SignIn and SignUp pages

--- src/app/routes/SignedInRoutes.tsx
+++ src/app/routes/SignedInRoutes.tsx
@@ -2,9 +2,12 @@ import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { MainLayout } from "../layouts/MainLayout";
+import { AuthLayout } from "../layouts/AuthLayout";
 
 import Home from "@/pages/Home";
 import Advice from "@/pages/Advice";
+import SignUp from "@/pages/SignUp";
+import SignIn from "@/pages/SignIn";
 
 export const SignedInRoutes: FC = () => {
 	return (
@@ -12,6 +15,10 @@ export const SignedInRoutes: FC = () => {
 			<Route path="/" element={<MainLayout />}>
 				<Route index element={<Home />} />
 				<Route path="advice" element={<Advice />} />
+			</Route>
+			<Route path="/auth" element={<AuthLayout />}>
+				<Route index element={<SignUp />} />
+				<Route path="signIn" element={<SignIn />} />
 			</Route>
 		</Routes>
 	);
