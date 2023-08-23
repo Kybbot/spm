884d0f6ebeb6349b81fb42128ffd0ae0da5259e9
Артём Одийко <odiykoartem@gmail.com>
2023-08-23 12:26:25 +0300

    Installed @tanstack/react-query

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,21 @@
 import { FC } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { queryClientConfig } from "./config/queryClientConfig";
 
 import { SignedInRoutes } from "./routes/SignedInRoutes";
+
+const queryClient = new QueryClient(queryClientConfig);
 
 export const App: FC = () => {
 	return (
 		<BrowserRouter>
-			<SignedInRoutes />
+			<QueryClientProvider client={queryClient}>
+				<SignedInRoutes />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
 		</BrowserRouter>
 	);
 };
