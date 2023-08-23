0dce0fb2d09546e656637a0cddc33ca6797b071b
Артём Одийко <odiykoartem@gmail.com>
2023-08-23 12:08:13 +0300

    Updated Button styles

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -2,12 +2,13 @@ import { FC, ButtonHTMLAttributes, ReactNode } from "react";
 
 type ButtonProps = {
 	children: ReactNode;
-	width: string;
+	width?: string;
+	color?: "green" | "red";
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button: FC<ButtonProps> = ({ children, width }) => {
+export const Button: FC<ButtonProps> = ({ children, width, color }) => {
 	return (
-		<button className="btn" style={{ width: width }}>
+		<button className={`btn ${color ? `btn--${color}` : ""}`} style={{ width: width ? width : "" }}>
 			{children}
 		</button>
 	);
